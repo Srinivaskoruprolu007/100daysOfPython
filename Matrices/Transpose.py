717363a6cbadba7557c058d17738efd96cc528db
Srinivaas <109063223+Srinivaskoruprolu007@users.noreply.github.com>
2023-09-05 13:00:33 +0530

    transpose modified and fixed

--- Matrices/Transpose.py
+++ Matrices/Transpose.py
@@ -1,13 +1,12 @@
 def make_trans(A):
     # no_of_rows
     rows = len(A)
+    cols = len(A[0])
+    trans = [[0 for _ in range(rows)] for _ in range(cols)]
     for i in range(rows):
-        for j in range(i):
-            temp = A[i][j]
-            A[i][j] = A[j][i]
-            A[j][i] = temp
-
-    return A
+        for j in range(cols):
+            trans[j][i] = A[i][j]
+    return trans
 
 
 B = [
