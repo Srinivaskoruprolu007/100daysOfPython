157765dbe9ba881db70d0c6afeb3f6f9d845d0f4
Srinivaas <109063223+Srinivaskoruprolu007@users.noreply.github.com>
2024-09-14 10:23:27 +0530

    Update Range_function.py

--- Day05/Range_function.py
+++ Day05/Range_function.py
@@ -1,7 +1,7 @@
-# for loop with range
+# Loop through numbers from 1 to 10 with a step of 3
 for number in range(1, 11, 3):
     print(number)
-
+# Calculate the sum of numbers from 1 to 100
 total = 0
 for num in range(1, 101):
     total += num
