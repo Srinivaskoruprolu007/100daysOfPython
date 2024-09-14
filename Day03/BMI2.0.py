0099b8f5f21715632c9fbbce0751d7f7e6526f0c
Srinivaas <109063223+Srinivaskoruprolu007@users.noreply.github.com>
2024-09-14 08:14:08 +0530

    Update BMI2.0.py

--- Day03/BMI2.0.py
+++ Day03/BMI2.0.py
@@ -1,8 +1,19 @@
-print("Welcome to BMI calculator 2.0 ")
-height = float(input("Enter your height in m : "))
+# BMI Calculator 2.0
+
+# Print a welcome message for the user.
+print("Welcome to BMI calculator 2.0")
+
+# Get the user's height in meters and convert it to a float.
+height = float(input("Enter your height in m: "))
+
+# Get the user's weight in kilograms and convert it to an integer.
 weight = int(input("Enter your weight in kg: "))
 
-BMI = round((weight / (height ** 2)))
+# Calculate BMI using the formula: weight (kg) / height (m)^2
+# Round the result to the nearest integer.
+BMI = round(weight / (height ** 2))
+
+# Determine the BMI category based on the calculated BMI value and print the result.
 if BMI <= 18.5:
     print(f"Your BMI is {BMI}, You are underweight")
 elif 18.5 < BMI < 25:
