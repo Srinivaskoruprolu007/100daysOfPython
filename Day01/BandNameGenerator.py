090eb34b021de881b063cdac53c59de8ce7600c8
Srinivaas <109063223+Srinivaskoruprolu007@users.noreply.github.com>
2024-09-14 08:00:14 +0530

    Update BandNameGenerator.py

--- Day01/BandNameGenerator.py
+++ Day01/BandNameGenerator.py
@@ -1,4 +1,22 @@
+# Band Name Generator Program
+
+# The program prompts the user for two inputs:
+# 1. The name of the place they grew up.
+# 2. The name of their pet.
+
+# It then combines these two inputs to suggest a possible band name.
+
+# Print the title of the program.
 print("---Band name generator---")
+
+# Get the name of the place the user grew up.
+# The input() function prompts the user and stores the response in the variable `area`.
 area = input("Enter the name of the place that you grown up ? ")
-pet_name = input("Enter the name of your pet ?")
-print("Your band name could be "+area + " "+pet_name)
+
+# Get the name of the user's pet.
+# The input() function prompts the user and stores the response in the variable `pet_name`.
+pet_name = input("Enter the name of your pet ? ")
+
+# Combine the area and pet_name to suggest a band name.
+# The + operator concatenates the strings with a space in between.
+print("Your band name could be " + area + " " + pet_name)
