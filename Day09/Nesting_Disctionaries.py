6f921225a8557082d093de363dff411ebf7c1fda
Srinivaas <109063223+Srinivaskoruprolu007@users.noreply.github.com>
2024-09-14 10:34:42 +0530

    Update Nesting_Disctionaries.py

--- Day09/Nesting_Disctionaries.py
+++ Day09/Nesting_Disctionaries.py
@@ -1,34 +1,42 @@
-# Nesting
+# Define a basic dictionary with country-capital pairs
 capitals = {
     "France": "Paris",
     "Germany": "Berlin"
 }
 
-# Nesting a List in a dictionary
-
+# Nesting a List in a Dictionary
+# This dictionary stores a list of cities visited in each country
 travel_log = {
     "France": ["Paris", "Lille", "Dijon"],
     "Germany": ["Berlin", "Hamburg", "Stuttgart"],
 }
 
-# Nesting a dictionary in a dictionary
+# Nesting a Dictionary in a Dictionary
+# This dictionary stores detailed information about visits to each country
+# Each country has a dictionary with cities visited and total visits
 travel_log = {
-    "France": {"cities_visited": ["Paris", "Lille", "Dijon"], "total_visits": 12},
-
-    "Germany": {"cities_visited": ["Berlin", "Hamburg", "stuttgart"], "total_visits": 5}
+    "France": {
+        "cities_visited": ["Paris", "Lille", "Dijon"],
+        "total_visits": 12
+    },
+    "Germany": {
+        "cities_visited": ["Berlin", "Hamburg", "Stuttgart"],
+        "total_visits": 5
+    }
 }
-# Nesting a dictionary in a list
+
+# Nesting a Dictionary in a List
+# This list contains dictionaries, each with details about a country
+# Each dictionary has keys for the country, cities visited, and total visits
 travel_log = [
     {
         "country": "France",
         "cities_visited": ["Paris", "Lille", "Dijon"],
         "total_visits": 12
-     },
-
+    },
     {
         "country": "Germany",
-        "cities_visited": ["Berlin", "Hamburg", "stuttgart"],
+        "cities_visited": ["Berlin", "Hamburg", "Stuttgart"],
         "total_visits": 5
     }
 ]
-
