eb18c3c5cb10c84a39076ce7930ac3c24fc12b85
Srinivaas <109063223+Srinivaskoruprolu007@users.noreply.github.com>
2024-09-14 10:15:58 +0530

    Update heads or tails.py

--- Day04/heads or tails.py
+++ Day04/heads or tails.py
@@ -1,6 +1,12 @@
 import random
+
+# Generate a random integer between 0 and 1 (inclusive).
 random_side = random.randint(0, 1)
+
+# Determine the outcome based on the generated random number.
 if random_side == 1:
+    # If the number is 1, print "tails".
     print("tails")
 else:
+    # If the number is 0, print "heads".
     print("heads")
