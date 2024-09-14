afcb12e0d0e9623c488ff90399dd84918ae65260
Srinivaas <109063223+Srinivaskoruprolu007@users.noreply.github.com>
2024-09-14 10:11:37 +0530

    Update Lists.py

--- Day04/Lists.py
+++ Day04/Lists.py
@@ -1,5 +1,14 @@
+# List of fruits
 fruits = ["Apple", "Banana", "Guava", "Strawberry"]
+
+# Print the first fruit in the list.
 print(fruits[0])
+
+# Replace the first fruit with "Mango".
 fruits[0] = "Mango"
+
+# Add "Pineapple" to the end of the list.
 fruits.append("Pineapple")
+
+# Print the updated list of fruits.
 print(fruits)
