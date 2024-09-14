01a63fa1bf38d252dccaf6377f294bcc83925d92
Srinivaas <109063223+Srinivaskoruprolu007@users.noreply.github.com>
2024-09-14 08:01:36 +0530

    Update DataTypes.py

--- Day02(Data types)/DataTypes.py
+++ Day02(Data types)/DataTypes.py
@@ -1,23 +1,38 @@
-# Data types
+# Data Types in Python
 
-
-# String
+# String:
+# A string is a sequence of characters enclosed within quotes.
+# Here, user_name is a string containing the value "Hello".
 user_name = "Hello"
+
+# You can access individual characters of a string using indexing.
+# Negative indexing starts from the end of the string.
+# This prints the last character of the string "Hello", which is 'o'.
 print(user_name[-1])
 
+# You can concatenate (join) strings using the + operator.
+# This will print "123345" as both values are strings.
 print("123" + "345")
 
 
-# Integer
+# Integer:
+# Integers are whole numbers, positive or negative, without decimals.
+# This will print the result of adding 123 and 345.
 print(123 + 345)
+
+# You can use underscores to make large numbers more readable.
+# Python ignores the underscores, so this will print 12345.
 print(12_345)
 
-# Float
+
+# Float:
+# A float represents a real number that includes a decimal point.
+# Here, PI is a float with the value 3.141.
 PI = 3.141
 
 
-# Boolean
+# Boolean:
+# A boolean is a data type that can have one of two values: True or False.
+# Variables `t` and `f` store boolean values True and False, respectively.
 t = True
 f = False
-
-
