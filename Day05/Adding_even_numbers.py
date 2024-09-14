c1a06c44466aef6fb56d95c1b98eed4de369549a
Srinivaas <109063223+Srinivaskoruprolu007@users.noreply.github.com>
2024-09-14 10:16:41 +0530

    Update Adding_even_numbers.py

--- Day05/Adding_even_numbers.py
+++ Day05/Adding_even_numbers.py
@@ -1,6 +1,13 @@
-print("Adding even numbers from 1 to 100 ")
-total = 0
-for number in range(2, 101, 2):
-    total += number
-print(total)
+print("Adding even numbers from 1 to 100")
 
+# Initialize the total sum to 0.
+total = 0
+
+# Iterate over the range of even numbers from 2 to 100 (inclusive).
+# The range function is set with a step of 2 to ensure only even numbers are considered.
+for number in range(2, 101, 2):
+    # Add the current even number to the total sum.
+    total += number
+
+# Print the final total sum of even numbers.
+print(total)
