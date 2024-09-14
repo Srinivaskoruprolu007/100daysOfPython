1850db24da56bb0f9653aa2ee65de843f94e0c29
Srinivaas <109063223+Srinivaskoruprolu007@users.noreply.github.com>
2024-09-14 10:15:21 +0530

    Update WhoPayTheBill.py

--- Day04/WhoPayTheBill.py
+++ Day04/WhoPayTheBill.py
@@ -1,11 +1,15 @@
 import random
 
-name_string = input("Give me everybody's names, separated by comma. ")
+# Prompt the user to input names separated by commas.
+name_string = input("Give me everybody's names, separated by comma: ")
+
+# Split the input string into a list of names.
 names = name_string.split(", ")
+
+# Randomly select a person to pay the bill using the randint function.
 payer = names[random.randint(0, len(names) - 1)]
-print(f"{payer} will be pay the bill.")
+print(f"{payer} will pay the bill.")
 
-# another approach to solve this : using choice in 'random' module
+# Another approach: Randomly select a person using the choice function.
 bill_payer = random.choice(names)
-print(f"{bill_payer} will be pay the bill.")
-
+print(f"{bill_payer} will pay the bill.")
