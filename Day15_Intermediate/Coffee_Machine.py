e3281bf1a660b8d773e3151ce0c74c8436297aef
Srinivaas <109063223+Srinivaskoruprolu007@users.noreply.github.com>
2024-09-14 17:30:08 +0530

    Update Coffee_Machine.py

--- Day15_Intermediate/Coffee_Machine.py
+++ Day15_Intermediate/Coffee_Machine.py
@@ -1,16 +1,26 @@
 from Coffee_Machine_Data import resources, profit, MENU
 
-
 def is_resources_sufficient(order_ingredients):
+    """
+    Checks if there are enough resources to make the requested drink.
+    
+    :param order_ingredients: dict, ingredients required for the drink
+    :return: bool, True if resources are sufficient, False otherwise
+    """
     for item in order_ingredients:
         if order_ingredients[item] >= resources[item]:
             print(f"Sorry, there is not enough {item}")
             return False
     return True
 
-
 def isTransaction_Successful(money_received, drink_cost):
-    """Return True when the payment is accepted, or False if money is insufficient"""
+    """
+    Checks if the payment is sufficient for the requested drink.
+    
+    :param money_received: float, amount of money inserted
+    :param drink_cost: float, cost of the drink
+    :return: bool, True if the transaction is successful, False otherwise
+    """
     if money_received >= drink_cost:
         change = round(money_received - drink_cost, 2)
         print(f"Here is the ${change} in change.")
@@ -21,16 +31,23 @@ def isTransaction_Successful(money_received, drink_cost):
         print("Sorry that's not enough money. Money Refunded")
         return False
 
-
 def make_coffee(drink_name, order_ingredients):
-    """Deduct th required ingredients from the resources."""
+    """
+    Deducts the required ingredients from the resources and prepares the coffee.
+    
+    :param drink_name: str, name of the drink to be made
+    :param order_ingredients: dict, ingredients required for the drink
+    """
     for item in order_ingredients:
         resources[item] -= order_ingredients[item]
     print(f'Here is your {drink_name} ☕')
 
-
 def process_coins():
-    """Returns the total calculated from coins inserted"""
+    """
+    Calculates the total amount of money inserted by the user.
+    
+    :return: float, total amount of money inserted
+    """
     print("Please insert coins.")
     total = int(input("How many quarters?: ")) * 0.25
     total += int(input("How many dimes?: ")) * 0.1
@@ -38,8 +55,11 @@ def process_coins():
     total += int(input("How many pennies?: ")) * 0.01
     return total
 
-
 def Coffee_Machine():
+    """
+    Main function to run the coffee machine program. It handles user input,
+    processes orders, and interacts with the other functions to serve coffee.
+    """
     is_on = True
 
     while is_on:
@@ -59,5 +79,5 @@ def Coffee_Machine():
                 if isTransaction_Successful(payment, drink['cost']):
                     make_coffee(choice, drink['ingredients'])
 
-
+# Start the coffee machine
 Coffee_Machine()
