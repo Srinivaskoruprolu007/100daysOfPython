5617c9c841653ca88a1ec6cd9b3f3ebed8ae90be
Srinivaas <109063223+Srinivaskoruprolu007@users.noreply.github.com>
2024-09-14 08:15:50 +0530

    Update Even or Odd.py

--- Day03/Even or Odd.py
+++ Day03/Even or Odd.py
@@ -1,8 +1,15 @@
-print("Welcome to Even and Odd Number checker!")
-number = int(input("Which number do you want : "))
+# Even and Odd Number Checker Program
 
+# Print a welcome message for the user.
+print("Welcome to Even and Odd Number Checker!")
+
+# Get the number from the user and convert it to an integer.
+number = int(input("Which number do you want: "))
+
+# Use the modulus operator (%) to check if the number is even or odd.
+# If the number modulo 2 equals 0, it is even.
 if number % 2 == 0:
-    print(f"{number} is even number ")
+    print(f"{number} is an even number.")
 else:
-    print(f"{number} is an odd number")
-    
+    # If the number modulo 2 does not equal 0, it is odd.
+    print(f"{number} is an odd number.")
