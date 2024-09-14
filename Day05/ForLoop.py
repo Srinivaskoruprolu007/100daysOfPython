d733ba7edbf1fb76cfe7f1a9189458fc05139707
Srinivaas <109063223+Srinivaskoruprolu007@users.noreply.github.com>
2024-09-14 10:19:19 +0530

    Update ForLoop.py

--- Day05/ForLoop.py
+++ Day05/ForLoop.py
@@ -1,6 +1,12 @@
+# Define a list of fruits.
 fruits = ["Apple", "Peach", "Pear"]
-for fruit in fruits:
-    print(fruit)
-    print(fruit + "Pie")
-print(fruits)
 
+# Iterate over each fruit in the list.
+for fruit in fruits:
+    # Print the name of the current fruit.
+    print(fruit)
+    # Print the name of the fruit with "Pie" appended.
+    print(fruit + "Pie")
+
+# Print the entire list of fruits.
+print(fruits)
