49a23a3b2e80f96877fd881733a0c528506b9304
Srinivaas <109063223+Srinivaskoruprolu007@users.noreply.github.com>
2024-09-14 07:59:07 +0530

    Update print.py

--- Day01/print.py
+++ Day01/print.py
@@ -1,16 +1,34 @@
-import  sys
+import sys
+
+# Basic Print Statement:
+# This prints the string "Hello World !" to the console.
+# The print() function outputs whatever is passed to it as a string.
 print("Hello World !")
+
+# Printing Multiple Lines:
+# The \n represents a newline character, causing the text after it to be printed on the next line.
 print("Hello World ! \nHello World !")
-# sep params
+
+# Using `sep` Parameter in `print`:
+# The `sep` parameter specifies how to separate the values. By default, the separator is a space " ".
+# In this case, sep="@" will print Hello@gmail.com.
 print("Hello", "gmail.com", sep="@")
 
+# String Concatenation:
+# The `+` operator is used to concatenate two strings without adding any space.
+# Output: "Hello Srinivas"
+print("Hello" + " Srinivas")
 
-# concat two strings
-print("Hello"+" Srinivas")
-
+# Concatenation with User Input:
+# The input() function prompts the user for input.
+# The value provided by the user is concatenated with "Hello " and printed.
+# Example: If the user inputs "Srinivas", the output would be `Hello Srinivas`.
 print("Hello " + input("What is your name ?"))
 
-# length of string
+# Getting the Length of a String (with Memory Size):
+# The input() function stores the user-provided value in the variable `name`.
+# The sys.getsizeof() function returns the size of an object in memory (in bytes).
+# This doesn't return the length of the string but rather the memory size occupied by the string object.
 name = input("Enter your name ?")
 size = sys.getsizeof(name)
 print(size)
