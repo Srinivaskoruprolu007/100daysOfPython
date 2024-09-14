340b5b7e0d8b85a2ca729b8026ce06410a7901a3
Srinivaas <109063223+Srinivaskoruprolu007@users.noreply.github.com>
2024-09-14 10:35:18 +0530

    Update Student_Grading.py

--- Day09/Student_Grading.py
+++ Day09/Student_Grading.py
@@ -1,3 +1,4 @@
+# Dictionary with student names and their scores
 student_scores = {
     'Harry': 81,
     'Ron': 78,
@@ -5,9 +6,16 @@ student_scores = {
     'Draco': 74,
     'Neville': 62,
 }
+
+# Create an empty dictionary to store student grades
 student_grades = {}
+
+# Iterate through each student in the student_scores dictionary
 for key in student_scores:
+    # Get the score for the current student
     score = student_scores[key]
+    
+    # Determine the grade based on the score and assign it to the student_grades dictionary
     if score > 90:
         student_grades[key] = "Outstanding"
     elif score > 80:
@@ -17,5 +25,5 @@ for key in student_scores:
     else:
         student_grades[key] = "Fail"
 
+# Print the dictionary containing student names and their respective grades
 print(student_grades)
-
