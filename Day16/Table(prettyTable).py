e72b995eb349c60c7f363e4067c10609e54d34eb
Srinivaas <109063223+Srinivaskoruprolu007@users.noreply.github.com>
2024-09-14 17:31:26 +0530

    Update Table(prettyTable).py

--- Day16/Table(prettyTable).py
+++ Day16/Table(prettyTable).py
@@ -1,5 +1,11 @@
 from prettytable import PrettyTable
+
+# Create a new PrettyTable object
 table = PrettyTable()
+
+# Add columns to the table with alignment to the right
 table.add_column("Pokemon Name", ["pikachu", "Squirrel", "fichu"], align='r')
 table.add_column("Type", ["Electric", "Water", "Fire"], align='r')
+
+# Print the table
 print(table)
