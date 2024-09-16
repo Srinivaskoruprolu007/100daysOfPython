7bb62db95dceb23b90e0d207cf94a799ca625f40
Srinivas <109063223+Srinivaskoruprolu007@users.noreply.github.com>
2024-09-16 06:39:29 +0530

    Update main.py

--- Day17/main.py
+++ Day17/main.py
@@ -2,6 +2,7 @@ from question_model import Question
 from Question_Data import question_data
 from quiz_brain import QuizBrain
 
+# Create a list of Question objects from the question data
 question_bank = []
 for question in question_data:
     question_text = question['text']
@@ -9,12 +10,14 @@ for question in question_data:
     new_question = Question(question_text, question_answer)
     question_bank.append(new_question)
 
-
+# Initialize the quiz with the question bank
 quiz = QuizBrain(question_bank)
 quiz.next_question()
 
-while quiz.sill_has_a_question():
+# Continue asking questions while there are more questions in the quiz
+while quiz.still_has_a_question():
     quiz.next_question()
 
+# Print the final results after the quiz is completed
 print("You've completed the quiz")
 print(f"Your final score was: {quiz.score}/{quiz.question_no}")
