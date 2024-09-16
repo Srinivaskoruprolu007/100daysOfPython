99063dacae71562acddce3a04a72910417dd855b
Srinivas <109063223+Srinivaskoruprolu007@users.noreply.github.com>
2024-09-16 06:37:28 +0530

    Update question_model.py

--- Day17/question_model.py
+++ Day17/question_model.py
@@ -1,5 +1,5 @@
 class Question:
+    # Initialize the Question class with the question text and the correct answer
     def __init__(self, text, answer):
-        self.text = text
-        self.answer = answer
-
+        self.text = text  # Stores the question text
+        self.answer = answer  # Stores the correct answer (True/False)
